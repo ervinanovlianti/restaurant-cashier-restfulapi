65eafeafc7d057fdf0ba6e491a1c03a8686c77c3
ervinanovlianti <ervinanov03@gmail.com>
2023-11-12 00:22:30 +0800

    update route

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -4,6 +4,6 @@ const menuController = require('../controller/menuController');
 
 // Rute untuk menampilkan menu
 router.get('/menus', menuController.getMenu);
-router.post('/menus', menuController.addMenuItem);
+router.post('/menu', menuController.addMenuItem);
 
 module.exports = router;
